b242be77ed9edce1e29d84eb246c32e4e4531c64
opticalfalicy <opticalfalicy@gmail.com>
2019-12-14 11:27:34 -0500

    initialize structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import './index.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>MARS</h1>
+
+      <div className="data">
+        <ul className='data-list'>
+          <li className='data-item at'>Temperature</li>
+          <li className='data-item hws'>Wind Speed</li>
+          <li className='data-item pre'>Atmospheric Pressure</li>
+          <li className='data-item season'>Season</li>
+          <li className='data-item last-utc'>Time of Last Datum</li>
+        </ul>
+      </div>
     </div>
   );
 }
